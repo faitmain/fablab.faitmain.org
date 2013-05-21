056dba748f955368e29fcad9cdf0d64b4da13b63
Tarek Ziade <tarek@ziade.org>
2013-05-21 22:58:48 +0200

    help!

--- fablabs.js
+++ fablabs.js
@@ -1,6 +1,6 @@
-
+// add a line into this Array
+// the two first values are your coordinates and the third the popup text.
 var fablabs = [
   [48.8567, 2.3508, "Tarek's House <a href='http://ziade.org'>http://ziade.org</a>"],
-
 
 ]
