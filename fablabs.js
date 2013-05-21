07d9c123927934956936393fbb7f1861eb594b63
Tarek Ziade <tarek@ziade.org>
2013-05-21 23:31:18 +0200

    added carrefour numerique

--- fablabs.js
+++ fablabs.js
@@ -2,5 +2,5 @@
 // the two first values are your coordinates and the third the popup text.
 var fablabs = [
   [48.8567, 2.3508, "Tarek's House <a href='http://ziade.org'>http://ziade.org</a>"],
-
+  [48.5345, 2.2313, "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a>"],
 ]
