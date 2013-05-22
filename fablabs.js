0bfc3a345171556026d932742cbb3b695fd77faf
Tarek Ziade <tarek@ziade.org>
2013-05-22 10:16:34 +0200

    typo

--- fablabs.js
+++ fablabs.js
@@ -5,6 +5,6 @@ var fablabs = [
   [49.0954485, 6.245274, "<a href='http://www.graoulab.org/'>GraouLab</a> - Metz"],
   [51.330364, 12.330839, "<a href='http://www.sublab.org/'>sublab</a>"],
   [44.915676, 4.915261, "<a href='http://l0ad.org/'>L0ad</a>"],
-  [47.22933, -1.58458, "<a href='http://www.pingbase.net/'>PiNG</a>"]
+  [47.22933, -1.58458, "<a href='http://www.pingbase.net/'>PiNG</a>"],
   [47.32303, 5.04036, "<a href='http://coagul.org/drupal/association/presentation-fablab-labscisse'>L'abscisse</a>"]
 ]
