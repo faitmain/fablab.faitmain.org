add33d33c08d61216baf1e1aebccdfef68d4daa4
Tarek Ziade <tarek@ziade.org>
2013-05-27 23:39:32 +0200

    added events

---
+++ events.js
@@ -0,0 +1,32 @@
+// add a line into this Array
+// the first value is the index from https://github.com/faitmain/fablab.faitmain.org/blob/master/fablabs.js
+// the second the date
+// the third is the title
+// the fourth the URL
+var events = [
+ [2, '2013-06-07', 'Apéro Pi - Metz (aPiro)', 'http://www.raspberrypi.org/phpBB3/viewtopic.php?f=65&t=39940'],
+ ]
+
+var weekday = new Array(7);
+weekday[0] = "Monday";
+weekday[1] = "Tuesday";
+weekday[2] = "Wenesday";
+weekday[3] = "Thursday";
+weekday[4] = "Friday";
+weekday[5] = "Saturday";
+weekday[6] = "Sunday";
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December" ];
+
+function _sort(ev1, ev2) {
+  var evdate1 = new Date(ev1[1]);
+  var evdate2 = new Date(ev2[1]);
+  if (ev1 > ev2) return 1;
+  if (ev1 < ev2) return -1;
+  return 0;
+}
+
+    events.sort(_sort);
+
+
--- fablabs.js
+++ fablabs.js
@@ -1,13 +1,16 @@
 // add a line into this Array
-// the two first values are your coordinates and the third the popup text.
+// the first value is the index
+// the two next values are your coordinates 
+// the last one the popup text (it can contain html)
+
 var fablabs = [
-  [48.5345, 2.2313, "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a>"],
-  [49.0954485, 6.245274, "<a href='http://www.graoulab.org/'>GraouLab</a> - Metz"],
-  [51.330364, 12.330839, "<a href='http://www.sublab.org/'>sublab</a>"],
-  [44.915676, 4.915261, "<a href='http://l0ad.org/'>L0ad</a>"],
-  [47.22933, -1.58458, "<a href='http://www.pingbase.net/'>PiNG</a>"],
-  [47.2217115, -1.5572179, "<a href='http://naohack.org/'>Naohack</a>"],
-  [47.32303, 5.04036, "<a href='http://coagul.org/drupal/association/presentation-fablab-labscisse'>L'abscisse</a>"],
-  [48.106679, -1.651168, "<a href='http://breizh-entropy.org/'>Breizh Entropy</a>"],
-  [48.114706, -1.677212, "<a href='http://labfab.fr/'>Labfab</a>"]
+  [1, 48.5345, 2.2313, "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a>"],
+  [2, 49.0954485, 6.245274, "<a href='http://www.graoulab.org/'>GraouLab</a> - Metz"],
+  [3, 51.330364, 12.330839, "<a href='http://www.sublab.org/'>sublab</a>"],
+  [4, 44.915676, 4.915261, "<a href='http://l0ad.org/'>L0ad</a>"],
+  [5, 47.22933, -1.58458, "<a href='http://www.pingbase.net/'>PiNG</a>"],
+  [6, 47.2217115, -1.5572179, "<a href='http://naohack.org/'>Naohack</a>"],
+  [7, 47.32303, 5.04036, "<a href='http://coagul.org/drupal/association/presentation-fablab-labscisse'>L'abscisse</a>"],
+  [8, 48.106679, -1.651168, "<a href='http://breizh-entropy.org/'>Breizh Entropy</a>"],
+  [9, 48.114706, -1.677212, "<a href='http://labfab.fr/'>Labfab</a>"]
 ]
