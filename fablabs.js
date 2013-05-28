b012ee59bd9c2e97a83f789e14d114cc4baaa832
Tarek Ziade <tarek@ziade.org>
2013-05-28 22:27:39 +0200

    use a cleaner data structure

--- fablabs.js
+++ fablabs.js
@@ -1,17 +1,13 @@
 // add a line into this Array
-// the first value is the index
-// the two next values are your coordinates
-// the last one the popup text (it can contain html)
-
 var fablabs = [
-  [1, 48.5345, 2.2313, "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a>"],
-  [2, 49.0954485, 6.245274, "<a href='http://www.graoulab.org/'>GraouLab</a> - Metz"],
-  [3, 51.330364, 12.330839, "<a href='http://www.sublab.org/'>sublab</a>"],
-  [4, 44.915676, 4.915261, "<a href='http://l0ad.org/'>L0ad</a>"],
-  [5, 47.22933, -1.58458, "<a href='http://www.pingbase.net/'>PiNG</a>"],
-  [6, 47.2217115, -1.5572179, "<a href='http://naohack.org/'>Naohack</a>"],
-  [7, 47.32303, 5.04036, "<a href='http://coagul.org/drupal/association/presentation-fablab-labscisse'>L'abscisse</a>"],
-  [8, 48.106679, -1.651168, "<a href='http://breizh-entropy.org/'>Breizh Entropy</a>"],
-  [9, 48.114706, -1.677212, "<a href='http://labfab.fr/'>Labfab</a>"],
-  [10, 48.019401, 0.157092, "<a href='http://haum.org/'>HAUM</a>"]
+  {'lat': 48.5345, 'lon': 2.2313, 'desc': "<a href='http://www.cite-sciences.fr/fr/carrefour-numerique'>Carrefour Numerique</a>"},
+  {'lat':49.0954485, 'lon': 6.245274, 'desc': "<a href='http://www.graoulab.org/'>GraouLab</a>"},
+  {'lat': 51.330364, 'lon': 12.330839, 'desc': "<a href='http://www.sublab.org/'>sublab</a>"},
+  {'lat': 44.915676, 'lon': 4.915261, 'desc': "<a href='http://l0ad.org/'>L0ad</a>"},
+  {'lat': 47.22933, 'lon': -1.58458, 'desc': "<a href='http://www.pingbase.net/'>PiNG</a>"},
+  {'lat': 47.2217115, 'lon': -1.5572179, 'desc': "<a href='http://naohack.org/'>Naohack</a>"},
+  {'lat': 47.32303, 'lon': 5.04036, 'desc': "<a href='http://coagul.org/drupal/association/presentation-fablab-labscisse'>L'abscisse</a>"},
+  {'lat': 48.106679, 'lon': -1.651168, 'desc': "<a href='http://breizh-entropy.org/'>Breizh Entropy</a>"},
+  {'lat': 48.114706, 'lon': -1.677212, 'desc': "<a href='http://labfab.fr/'>Labfab</a>"},
+  {'lat': 48.019401, 'lon': 0.157092, 'desc': "<a href='http://haum.org/'>HAUM</a>"}
 ]
