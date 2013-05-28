27918fe23a11e8abcaa9ef8e31fd6abd88896269
Tarek Ziade <tarek@ziade.org>
2013-05-28 21:53:37 +0200

    added widget

--- events.js
+++ events.js
@@ -1,36 +1,28 @@
-// add a line into this Array
-// the first value is the index from https://github.com/faitmain/fablab.faitmain.org/blob/master/fablabs.js
-// the second the date
-// the third is the title
-// the fourth the URL
+// add a content into this Array
 var events = [
- [2, '2013-06-07', 'Apéro Pi - Metz (aPiro)', 'http://www.raspberrypi.org/phpBB3/viewtopic.php?f=65&t=39940'],
- [-1, '2013-06-16', 'le POOP', 'http://wiki.leloop.org/index.php/POOP'],
- [-1, '2013-06-23', 'Pas Sage En Seine', 'http://www.passageenseine.org/Passage/pses-2013'],
- [-1, '2013-06-22', 'Open Bidouille Camp', 'http://openbidouille.net'],
- [-1, '2013-07-08', 'SummerLab 2013', 'http://summerlabnantes.net']
- ]
+  {'date': '2013-06-07 20:00:00', 
+   'type': 'meeting', 
+   'title': 'Apéro Pi - Metz (aPiro)', 
+   'url': 'http://www.raspberrypi.org/phpBB3/viewtopic.php?f=65&t=39940'},
 
-var weekday = new Array(7);
-weekday[0] = "Monday";
-weekday[1] = "Tuesday";
-weekday[2] = "Wenesday";
-weekday[3] = "Thursday";
-weekday[4] = "Friday";
-weekday[5] = "Saturday";
-weekday[6] = "Sunday";
+  {'date': '2013-06-16 09:00:00', 
+   'type': 'meeting', 
+   'title': 'le POOP', 
+   'url': 'http://wiki.leloop.org/index.php/POOP'},
 
-var monthNames = ["January", "February", "March", "April", "May", "June",
-"July", "August", "September", "October", "November", "December" ];
+  {'date': '2013-06-22 09:00:00', 
+   'type': 'meeting', 
+   'title': 'Open Bidouille Camp', 
+   'url': 'http://openbidouille.net'},
 
-function _sort(ev1, ev2) {
-  var evdate1 = new Date(ev1[1]);
-  var evdate2 = new Date(ev2[1]);
-  if (ev1 > ev2) return 1;
-  if (ev1 < ev2) return -1;
-  return 0;
-}
+  {'date': '2013-06-23 09:00:00', 
+   'type': 'meeting', 
+   'title': 'Pas Sage En Seine', 
+   'url': 'http://www.passageenseine.org/Passage/pses-2013'},
 
-events.sort(_sort);
-
+  {'date': '2013-07-08 09:00:00', 
+   'type': 'meeting', 
+   'title': 'SummerLab 2013', 
+   'url': 'http://summerlabnantes.net'},
+]
 
